3badd66d8d1d060bc808e965b543f15eee0bd2cc
rajat404 <404rajat@gmail.com>
2016-08-05 02:57:08 +0530

    util funcs, and option to add paths added

--- mocount.py
+++ mocount.py
@@ -8,22 +8,13 @@ from pygments.style import Style
 from pygments.styles.default import DefaultStyle
 from pygments.token import Token
 
-import os
-import sqlite3
 import dataset
-import ptpdb
+import click
 
-from utils import create_directory
+from utils import create_project_directory, add_path, get_file_list
+from config import project_path, history_file, db_file, movie_paths
 
-
-project_path = '{}/.mocount'.format(os.path.expanduser('~'))
-# creates the project dir if it doesn't exists
-create_directory(project_path)
-
-history_file = '{}/search-history'.format(project_path)
-db_file = '{}/movies.db'.format(project_path)
 db = dataset.connect('sqlite:///{}'.format(db_file))
-db = dataset.connect()
 
 
 class DocumentStyle(Style):
@@ -36,9 +27,24 @@ class DocumentStyle(Style):
     styles.update(DefaultStyle.styles)
 
 
-def main():
-    history = FileHistory(history_file)
+@click.command()
+@click.argument('directories', nargs=-1, type=click.Path())
+def main(directories):
+    """
+    Enter the absolute paths of the directories containing your
+    Movies/TV Shows, separated by an empty space
+    """
+    # creates the project dir if it doesn't exists
+    create_project_directory(project_path)
+    if directories:
+        for directory in directories:
+            # Ensure that the directory path begins with '/', but doesn't end with one
+            path = '/{}'.format(directory.strip('/'))
+            if path not in movie_paths:
+                add_path(path)
 
+    print('Press Ctrl+D to exit at any time!')
+    history = FileHistory(history_file)
     while True:
         try:
             text = prompt('> ', style=DocumentStyle, history=history,
--- utils.py
+++ utils.py
@@ -1,14 +1,29 @@
-import inspect
 import os
-import shutil
+from config import movie_ext, movie_paths
 
-
-def create_directory(directory):
+def create_project_directory(directory):
     """
     Creates the specified directory if it does not exist
     """
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except Exception as e:
-        print('Error creating directory {} : {}'.format(directory, e))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def add_path(directory):
+    """
+    Adds the specified directory in the config as a movie path
+    """
+    if not os.path.exists(directory):
+        return False
+    movie_paths.append(directory)
+
+
+def get_file_list():
+    file_list = []
+    for directory in movie_paths:
+        for path, subdirs, files in os.walk(directory):
+            for filename in files:
+                name, ext = os.path.splitext(filename)
+                if ext.lower() in movie_ext:
+                    file_list.append(filename)
+    return file_list
