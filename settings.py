005e17df3b4fd668d33c3a9257c493cb3116670e
rajat404 <404rajat@gmail.com>
2017-01-02 15:11:00 +0530

    multiple utils added

--- settings.py
+++ settings.py
@@ -1,23 +1,59 @@
 import os
-import dataset
-from db import DB
+import logging
+# import dataset
+from playhouse.dataset import DataSet
 
-movie_ext = ['.3g2', '.3gp', '.3gp2', '.3gpp', '.60d', '.ajp', '.asf', '.asx',
-             '.avchd', '.avi', '.bik', '.bix', '.box', '.cam', '.dat', '.divx',
-             '.dmf', '.dv', '.dvr-ms', '.evo', '.flc', '.fli', '.flic', '.flv',
-             '.flx', '.gvi', '.gvp', '.h264', '.m1v', '.m2p', '.m2ts', '.m2v',
-             '.m4e', '.m4v', '.mjp', '.mjpeg', '.mjpg', '.mkv', '.moov',
-             '.mov', '.movhd', '.movie', '.movx', '.mp4', '.mpe', '.mpeg',
-             '.mpg', '.mpv', '.mpv2', '.mxf', '.nsv', '.nut', '.ogg', '.ogm',
-             '.omf', '.ps', '.qt', '.ram', '.rm', '.rmvb', '.swf', '.ts',
-             '.vfw', '.vid', '.video', '.viv', '.vivo', '.vob', '.vro', '.wm',
-             '.wmv', '.wmx', '.wrap', '.wvx', '.wx', '.x264', '.xvid']
+# for DB.py
+# from db import DB
+# db = DB(filename=db_file, dbname="flix", dbtype="sqlite")
+logger = logging.getLogger('sdl')
 
-movie_url = 'http://www.omdbapi.com/?'
+LOG_FILENAME = 'metaflix.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO,)
 
 
 project_path = '{}/.flix'.format(os.path.expanduser('~'))
 db_file = '{}/flix.db'.format(project_path)
 
-dataset_db = dataset.connect('sqlite:///{}'.format(db_file))
-db = DB(filename=db_file, dbname="flix", dbtype="sqlite")
+# dataset_db = dataset.connect('sqlite:///{}'.format(db_file))
+dataset_db = DataSet('sqlite:///{}'.format(db_file))
+
+media_api_url = 'http://www.omdbapi.com/?'
+
+# media_extenions = [
+#     '.3g2', '.3gp', '.3gp2', '.3gpp', '.60d', '.ajp', '.asf', '.asx',
+#     '.avchd', '.avi', '.bik', '.bix', '.box', '.cam', '.dat', '.divx',
+#     '.dmf', '.dv', '.dvr-ms', '.evo', '.flc', '.fli', '.flic', '.flv',
+#     '.flx', '.gvi', '.gvp', '.h264', '.m1v', '.m2p', '.m2ts', '.m2v',
+#     '.m4e', '.m4v', '.mjp', '.mjpeg', '.mjpg', '.mkv', '.moov',
+#     '.mov', '.movhd', '.movie', '.movx', '.mp4', '.mpe', '.mpeg',
+#     '.mpg', '.mpv', '.mpv2', '.mxf', '.nsv', '.nut', '.ogg', '.ogm',
+#     '.omf', '.ps', '.qt', '.ram', '.rm', '.rmvb', '.swf', '.ts',
+#     '.vfw', '.vid', '.video', '.viv', '.vivo', '.vob', '.vro', '.wm',
+#     '.wmv', '.wmx', '.wrap', '.wvx', '.wx', '.x264', '.xvid'
+# ]
+
+media_extenions = [
+    '.3gp', '.dat', '.divx', '.flv', '.m4v', '.mkv', '.mov', '.movhd',
+    '.mp4', '.mpeg', '.mpg', '.rm', '.rmvb', '.vob', '.wm', '.wmv', '.xvid'
+]
+
+response_mapping = [
+    # format: (original_key, new_key, result_data_type, default_value)
+    ('Actors', 'actors', str, ''),
+    ('Director', 'director', str, ''),
+    ('Genre', 'genre', str, ''),
+    ('Metascore', 'metascore', float, 0),
+    ('Plot', 'plot', str, ''),
+    ('Released', 'released', str, ''),
+    ('Runtime', 'runtime', str, ''),
+    ('Title', 'title', str, ''),
+    ('Type', 'media_type', str, ''),
+    ('Year', 'year', int, 0),
+    ('imdbID', 'imdb_id', str, ''),
+    ('imdbRating', 'rating', float, 0),
+    ('tomatoMeter', 'tomato_meter', float, 0),
+    ('tomatoURL', 'tomato_url', str, ''),
+    ('Poster', 'poster', str, ''),
+    ('Rated', 'rated', str, ''),
+]
--- utils.py
+++ utils.py
@@ -1,17 +1,31 @@
 import os
 import requests
 import json
+import glob
 from guessit import guessit
-from settings import movie_ext, dataset_db, db
+from settings import media_extenions, dataset_db, movie_api_url, response_mapping, logger
 
-movie_data = dataset_db['movie_data']
+import linecache
+import sys
+import traceback
+from decorator import decorator
 
-keys_to_keep = ['Actors', 'Director', 'Genre', 'Metascore', 'Plot', 'Released',
-                'Runtime', 'Title', 'Type', 'Year', 'imdbID', 'imdbRating',
-                'tomatoConsensus', 'tomatoMeter', 'tomatoURL',
-                'tomatoUserMeter']
 
-url = 'http://www.omdbapi.com/?'
+@decorator
+def print_error(fn, *args, **kwargs):
+    try:
+        return fn(*args, **kwargs)
+    except:
+        exc_type, exc_obj, tb = sys.exc_info()
+        f = tb.tb_frame
+        filename = f.f_code.co_filename
+        linecache.checkcache(filename)
+        traceback_text = ''.join(traceback.format_exception(*sys.exc_info()))
+        logger.error('EXCEPTION IN:\nFUNCTION: {}\nERROR: {}\nEXCEPTION: {}'.format(
+            fn.__name__, exc_obj, traceback_text))
+        print('EXCEPTION IN:\nFUNCTION: {}\nERROR: {}\nEXCEPTION: {}'.format(
+            fn.__name__, exc_obj, traceback_text))
+        print('-'*50)
 
 
 def create_project_directory(directory):
@@ -22,40 +36,85 @@ def create_project_directory(directory):
         os.makedirs(directory)
 
 
-def get_file_list():
-    all_paths = db.tables.movie_paths.unique('directory')['directory'].tolist()
+# def get_file_list(dir_list):
+#     # dir_list = db.tables.movie_paths.unique('directory')['directory'].tolist()
+#     file_list = []
+#     for directory in dir_list:
+#         for path, subdirs, files in os.walk(directory):
+#             # exclude hidden & temprary files
+#             files = [f for f in files if not f[0] in ('.', '~')]
+#             for filename in files:
+#                 name, ext = os.path.splitext(filename)
+#                 if ext.lower() in media_extenions:
+#                     file_list.append(filename)
+#     return file_list
+
+def get_file_list(dir_list):
     file_list = []
-    for directory in all_paths:
-        for path, subdirs, files in os.walk(directory):
-            # exclude hidden & temprary files
-            files = [f for f in files if not f[0] in ('.', '~')]
-            for filename in files:
-                name, ext = os.path.splitext(filename)
-                if ext.lower() in movie_ext:
-                    file_list.append(filename)
+    for directory in dir_list:
+        # TODO: Instead of all media_extenions, ask user which extensions to consider
+        file_list.extend(flatten([glob.glob('{}/*/**/*{}'.format(directory, ext), recursive=True)
+                                  for ext in media_extenions]))
     return file_list
 
 
 def fetch_movie_details(filename):
+    """
+    Takes the name of the media file and returns the metadata for the movie
+    """
     info = guessit(filename)
-    try:
-        # print('\n', info['title'])
-        params = {'t': info['title'].encode('ascii', 'ignore'),
-                  'type': info['type'],
-                  'tomatoes': 'true'}
-        if 'year' in info:
-            params['y'] = info['year']
+    # print('\n', info['title'])
+    params = {'t': info['title'].encode('ascii', 'ignore'),
+              'type': info['type'],
+              'tomatoes': 'true',
+              'plot': 'full',
+              'v': '1'}
+    if 'year' in info:
+        params['y'] = info['year']
 
-        resp = requests.get(url=url, params=params)
-        if resp.ok:
-            response = json.loads(resp.text)
-            temp = {k: response[k] for k in keys_to_keep if k in response}
-            temp['Filename'] = filename
-            # movie_data.insert(temp)
-            movie_data.upsert(temp, ['imdbID'])
-            return True
-        else:
-            return False
-    except Exception as e:
-        print(e)
-        return False
+    resp = requests.get(url=movie_api_url, params=params)
+    response = json.loads(resp.text)
+
+    # because resp.ok - is coming true for all cases
+    if response['Response'] == 'True':
+        temp = remap_response(response=response)
+        return (filename, temp)
+    return (filename, None)
+
+
+def save_media_details(filename, details):
+    """Takes the media details and saves in the DB"""
+    table = dataset_db['Media']
+    # temp['filename'] = filename
+    # movie_data.insert(temp)
+    table.insert(**details)
+
+
+def remap_response(response, response_mapping=response_mapping):
+    """
+    Takes the Media API response body, and returns a new dict,
+    with the keys and values as per `response_mapping`
+    """
+    result = {}
+    for original_key, new_key, result_data_type, default_value in response_mapping:
+        value = response.get(original_key)
+
+        if value == 'N/A':
+            value = default_value
+
+        result[new_key] = result_data_type(value)
+    return result
+
+
+def flatten(lis):
+    """
+    Takes a list (nested or regular), and returns a flat list
+    """
+    result = []
+    for item in lis:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        # won't accept empty dict or tuple
+        elif item not in [None, (), {}]:
+            result.append(item)
+    return result
