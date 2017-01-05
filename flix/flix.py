d47fda2b3be281fcd84f40dddeb413144a028c08
rajat404 <404rajat@gmail.com>
2017-01-05 16:09:44 +0530

    api working using flask-potion

--- flix/flix.py
+++ flix/flix.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return render_template('test.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+#     # return "Hello World!"
+
+
+# @app.route('/<name>')
+# def hello_name(name):
+#     return "Hello {}!".format(name)
+
+
+from flask import Flask
+from flask_potion import Api, ModelResource
+from flask_potion.contrib.peewee import PeeweeManager
+from .models import Media
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('test.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
-    # return "Hello World!"
+class MediaResource(ModelResource):
+    class Meta:
+        name = 'media'
+        model = Media
+        exclude_fields = ['created_at', 'id']
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
+api = Api(app, default_manager=PeeweeManager)
+api.add_resource(MediaResource)
