1f2cb2d6940a09fb0ae6ffb6811eda8d2392cb14
rajat404 <404rajat@gmail.com>
2017-01-11 15:48:37 +0530

    table changed to div

--- flix/flix.py
+++ flix/flix.py
@@ -7,9 +7,25 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 @app.route('/index')
 def index():
     all_media = Media.select()
     return render_template("index.html",
+                           title='Home',
+                           all_media=all_media)
+
+
+@app.route('/test2')
+def test2():
+    all_media = Media.select()
+    return render_template("test2.html",
+                           title='Home',
+                           all_media=all_media)
+
+@app.route('/test3')
+def test3():
+    all_media = Media.select()
+    return render_template("test3.html",
                            title='Home',
                            all_media=all_media)
